5ab442557fa374fd8fc6f33eb623e35deeb1cf8c
Habibur Rahman <habib@inverseai.com>
2021-07-14 12:03:32 +0600

    Add framework

---
+++ HREmailValidator/HREmailValidator/HREmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  HREmailValidator.h
+//  HREmailValidator
+//
+//  Created by Office on 14/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HREmailValidator.
+FOUNDATION_EXPORT double HREmailValidatorVersionNumber;
+
+//! Project version string for HREmailValidator.
+FOUNDATION_EXPORT const unsigned char HREmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HREmailValidator/PublicHeader.h>
+
+
---
+++ HREmailValidator/HREmailValidator/Validator.swift
@@ -0,0 +1,14 @@
+//
+//  Validator.swift
+//  HREmailValidator
+//
+//  Created by Office on 14/07/2021.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func sayHello() {
+        print("Hello")
+    }
+}
